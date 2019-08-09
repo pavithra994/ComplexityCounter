94eff1f55f8f6b463573bde5ec7edb85b0c78977
Pavithra <pavithra.blog@gmail.com>
2019-08-09 18:55:23 +0530

    add methodDivider function

--- model/complexity.py
+++ model/complexity.py
@@ -1,3 +1,6 @@
+import re
+
+
 class ComplexityFactory:
     def __init__(self, code):
         self.code = code
@@ -15,7 +18,7 @@ class ComplexityFactory:
             temp = temp_code.split(' ', 1)
             className = temp[0]
             temp_code = temp[1]
-            start, end = indexOfParenthesis(temp_code)
+            start, end = indexOfParenthesis(temp_code,0)
             classBody = temp_code[start:end]
             temp_code = temp_code[end:]
             self.classList.append(Class(className,classBody))
@@ -41,9 +44,19 @@ class ComplexityFactory:
 class Class:
     def __init__(self, name, body):
         self.className = name
-        self.body = body
+        # self.body = body
         self.methodList = []
         self.attributeList = []
+        self.methodDivider(body)
+
+    def methodDivider(self,body):
+        for indexes in re.finditer("\w*\s*\(.*\)\s*\{",body):
+            name = body[indexes.start():indexes.end()].split('(',1)[0]
+            # print(name)
+            s,e =indexOfParenthesis(body,indexes.end()-1)
+            # print(body[s:e])
+            # print('----')
+            self.methodList.append(Method(name,body[s:e]))
 
 
 class Method:
@@ -52,19 +65,37 @@ class Method:
         self.codeList = code_list
 
 
-def indexOfParenthesis(code):
+# def indexOfParenthesis(code):
+#     start_index = -1
+#     end_index = -1
+#     par = 0
+#     for i,char in enumerate(code):
+#         if char is '{':
+#             par += 1
+#             if par is 1:
+#                 start_index = i
+#         if char is '}':
+#             par -= 1
+#             if par is 0:
+#                 end_index = i+1
+#                 break
+#     return start_index, end_index
+
+
+def indexOfParenthesis(code, start):
     start_index = -1
     end_index = -1
     par = 0
     for i,char in enumerate(code):
-        if char is '{':
-            par += 1
-            if par is 1:
-                start_index = i
+        if i >= start:
+            # print(i)
+            if char is '{':
+                par += 1
+                if par is 1:
+                    start_index = i
             if char is '}':
                 par -= 1
                 if par is 0:
                     end_index = i+1
                     break
     return start_index, end_index
-
