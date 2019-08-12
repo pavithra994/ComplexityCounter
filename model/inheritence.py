c6d5109a206e8ab334691090109cff16f67b39fe
Pavithra Weerapperuma <36977042+pavithra994@users.noreply.github.com>
2019-08-12 23:48:30 +0530

    update ci

--- model/inheritence.py
+++ model/inheritence.py
@@ -21,3 +21,22 @@ def checkInheritence(i):
 
         else:
             return Class.className.match +1
+
+# ashimi try this
+    
+def calCi(class_obj,class_list):
+    if class_obj.parentClass == "Object":
+        return 2
+    else:
+        for cl in class_list:
+            if cl.className == class_obj.parentClass:
+                return calCi(cl, class_list) + 1
+
+c1 = Class("a","some text","Object")
+c2 = Class("b","some text","a")
+c3 = Class("c","some text","b")
+c4 = Class("d","some text","c")
+
+clist = [c1,c2,c3,c4]
+
+calCi(c3,clist)
