5bfa41de94f60603d146e11ba1deb917aae3cd29
Chathurindu <100wpck@gmail.com>
2019-08-15 13:45:22 +0530

    added 2 function

--- model/complexity.py
+++ model/complexity.py
@@ -173,7 +173,79 @@ def parentClassFinder(code):
             return keywords[i+1]
     return 'Object'
 
+############### Nested Control structure #################
 
+def lineCounterOfNestedController(code):
+    count = 0
+    stack = 0
+    line_number = 0
+
+    for num, line in enumerate(code,1):
+        # count += 1
+        if re.search('(if)\s*\(.*\)\s*\{',line):
+            # count -= 1
+            # count += 1
+            line_number = num
+            if re.search('\s*\(.*\)\s*\{',line):
+                stack += 1
+                
+        if re.search('\s*\}',line):
+            # print(line)
+            if stack == 1:
+                count = num - line_number
+                stack -= 1
+    return count
+
+# code = open("java.java")
+# print (lineCounterOfNestedController(code))
+            
+############### Type of Control structure #################
+
+def identifyControlStructure(code):
+    for num, line in enumerate(code,1):
+        if re.search('(if)\s*\(.*\)\s*\{',line):
+            Ctc = 0
+            Ctc += 1
+            print("In line number : ",num,"-> Ctc with only if : ",Ctc)
+            
+            if re.search("\s*\(.*(&&).*\)\s*{", line) or re.search("\s*\(.*\||\.\)\s",line):
+                Ctc += 1
+                print("In line number : ",num,"-> Ctc with if and && or ||: ",Ctc)
+
+        if re.search('(while)\s*\(.*\)\s*\{',line):
+            Ctc = 0
+            Ctc += 2
+            print("In line number : ",num,"-> Ctc with only while : ",Ctc)
+            
+            if re.search("\s*\(.*(&&).*\)\s*{", line) or re.search("\s*\(.*\||\.\)\s",line):
+                Ctc += 2
+                print("In line number : ",num,"-> Ctc with while and && or ||: ",Ctc)
+
+        if  re.search('(for)\s*\(.*\)\s*\{',line) :
+            Ctc = 0
+            Ctc += 2
+            print("In line number : ",num,"-> Ctc with only for : ",Ctc)
+            
+            if re.search("\s*\(.*(&&).*\)\s*{", line) or re.search("\s*\(.*\||\.\)\s",line):
+                Ctc += 2
+                print("In line number : ",num,"-> Ctc with for and && or ||: ",Ctc)
+
+        if  re.search('(do)\s*\{',line) :
+            Ctc = 0
+            Ctc += 2
+            print("In line number : ",num,"-> Ctc with only do while : ",Ctc)
+            
+            if re.search("\s*\(.*(&&).*\)\s*{", line) or re.search("\s*\(.*\||\.\)\s",line):
+                Ctc += 2
+                print("In line number : ",num,"-> Ctc with do while and && or ||: ",Ctc)
+
+        if  re.search('(catch)\s*\(.*\)\s*\{',line) :
+            Ctc = 0
+            Ctc += 1
+            print("In line number : ",num,"-> Ctc with catch : ",Ctc)
+
+# code = open("java.java")
+# print (identifyControlStructure(code))
 
 # testing
 
